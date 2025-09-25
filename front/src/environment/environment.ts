036127cb9bd8f0880da4f2433eb3d72f6aeb5db3
YuverHurtadoP <santiagohurtadopea@gmail.com>
2025-09-25 14:26:26 -0500

    avanes

--- front/src/environment/environment.ts
+++ front/src/environment/environment.ts
@@ -1,5 +1,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000/api'
+ // apiUrl: 'http://localhost:4000/api'
+
+ apiUrl: ' https://gestorpresupuesto-4.onrender.com/api'
+
 };
